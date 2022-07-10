470fe01cca970770fafeb07bfcbcd5654edf1d70
sobrelamarcha <sobrelamarcha@protonmail.com>
2022-07-10 10:20:18 +0200

    person model

---
+++ models/person.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const url = process.env.MONGODB_URI;
+
+console.log(`connecting to ${url}`);
+
+mongoose
+  .connect(url)
+  .then((result) => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("error connecting to MongoDB:", error.message);
+  });
+
+const personSchema = new mongoose.Schema({
+  name: { type: String, minlength: 3, required: true, unique: true },
+  phone: { type: String, minlength: 8, required: true },
+});
+
+personSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+module.exports = Person;
